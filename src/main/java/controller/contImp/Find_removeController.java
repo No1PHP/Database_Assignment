41e53c48dac0b4513862e46dcd9d115c2b9ca3f6
LAST-iMP <xiaoshibang@126.com>
2020-05-31 16:00:56 +0800

    add

---
+++ src/main/java/controller/contImp/Find_removeController.java
@@ -0,0 +1,53 @@
+package controller.contImp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static constants.globalConstants.LOGIN_STATUS;
+import static constants.globalConstants.SERVICE;
+
+@CrossOrigin(allowCredentials = "true")
+@Controller
+public class Find_removeController {
+
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> findAll(@RequestParam("pageNo") Integer pageNo, @RequestParam("size") Integer size, @RequestParam("page") String name) {
+        Map<String, Object> map = new HashMap<>();
+        if (LOGIN_STATUS) {
+            try {
+                map.put("result", SERVICE.getALL(name, size, pageNo));
+                map.put("succeed", true);
+            } catch (Exception e) {
+                map.put("succeed", false);
+                map.put("message: ", e.getMessage());
+            }
+        } else {
+            map.put("succeed", false);
+            map.put("message", "please login first!");
+        }
+        return map;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> removeByID(@RequestParam("id") Object id, @RequestParam("name") String name) {
+        Map<String, Object> map = new HashMap<>();
+        if (LOGIN_STATUS) {
+            try {
+                SERVICE.removeByID(id, name);
+                map.put("succeed", true);
+            } catch (Exception e) {
+                map.put("succeed", false);
+                map.put("message: ", e.getMessage());
+            }
+        } else {
+            map.put("succeed", false);
+            map.put("message", "please login first!");
+        }
+        return map;
+    }
+}
